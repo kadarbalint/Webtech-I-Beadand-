10b4b42115b3d932570db04c0d81208f9bbe3c99
kadarbalint <65283137+kadarbalint@users.noreply.github.com>
2020-12-15 11:56:32 +0100

    Update index.js

--- OYDQP1_Webtech beadando/js/index.js
+++ OYDQP1_Webtech beadando/js/index.js
@@ -1,7 +1,7 @@
 $(document).ready(() => {
     $("#content").load("home.html");
 
-    $.each($(".nav-link"), (index, value) => {
+    $.each($(".menuLink"), (index, value) => {
         $(value).click((e) => {
             e.preventDefault();
             $('#content').load(value.href);
